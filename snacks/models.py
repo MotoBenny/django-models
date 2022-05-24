6d64997d374ef66d532dd320a7f9892da3f47cf3
Benjamin <benj.james.carter@gmail.com>
2022-05-24 16:21:02 -0700

    inital branch commit

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Snack(models.Model):
+    name = models.CharField(max_length=64)  # relational DB wants to know how much memory to allocate
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.CharField(max_length=256)
+    # cascade means when the user goes away the data associated with the user goes away
+
+    def __str__(self):
+        return self.name  # so the name shows in the admin panel
